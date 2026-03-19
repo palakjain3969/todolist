d54dbcef1665a07b2fb0384c8c0042dfdddbc20d
palak <pjn797@gmail.com>
2026-03-20 00:30:36 +0530

    splash activity cahnged

--- app/src/main/java/com/example/todo/SplashActivity.java
+++ app/src/main/java/com/example/todo/SplashActivity.java
@@ -1,5 +1,6 @@
 package com.example.todo;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -15,10 +16,16 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        getSupportActionBar().hide();
+
+        //this intent will show the main activity
+        final Intent i = new Intent(SplashActivity.this, MainActivity.class);
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run(){
+                startActivity(i);
+                finish();
+            }
+        }, 1000);
     }
 }
